fbcec9d855e33cf15380a83610bbce87b639361b
Diana Khamatdiova <dianchick89@mail.ru>
2013-11-16 21:29:39 +0600

    Добавлен цикл do...while

--- khamatdinova/script.js
+++ khamatdinova/script.js
@@ -1,10 +1,17 @@
 var login = prompt("Have you login?");
 
 if (login == "admin") {
-	var pass = prompt("It's right login. Please input your pass:");
-	if (pass == "111"){
-		alert("It is top secret material!");
+		var i = 0;
+	do {
+		var pass = prompt("It's right login. Please input your pass:");
+	
+
+		if (pass == "111"){
+			alert("It is top secret material!"); break;
 		}
-	else {alert("Wrong pass");}
+		else {alert("Wrong pass"); i++;}
+	}
+	while (i<5);
+	alert("Authorization rejected. Sorry!");
 }
 else {alert("Wrong login");}
